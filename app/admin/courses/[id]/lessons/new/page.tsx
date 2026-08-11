2eb6b744ef1dac33c4c926d01aaf0a185b70271c
Alexogoth <Aleksandar.vasovicpg@gmail.com>
2026-08-11 23:23:01 +0200

    Add lesson placeholder page

---
+++ app/admin/courses/[id]/lessons/new/page.tsx
@@ -0,0 +1,9 @@
+export default function NewLessonPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-300">
+        Nova lekcija
+      </h1>
+    </main>
+  );
+}
